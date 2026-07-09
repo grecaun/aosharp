d7d1486b1e8b62e85af28eb1bbc6d4c01b36632e
James Sentinella <james@sentinella.me>
2026-07-09 16:11:24 -0700

    Added synchronized mounting for Mounter utility.

---
+++ Bots/Utilities/Mounter/IPCMessages.cs
@@ -0,0 +1,27 @@
+﻿using AOSharp.Core.IPC;
+using SmokeLounge.AOtomation.Messaging.Serialization.MappingAttributes;
+
+namespace Mounter
+{
+    public partial class Main
+    {
+        private enum IPCOpcode
+        {
+            Mount = 8700,
+            Dismount = 8701,
+        }
+
+
+        [AoContract((int)IPCOpcode.Mount)]
+        private class MountMessage : IPCMessage
+        {
+            public override short Opcode => (short)IPCOpcode.Mount;
+        }
+
+        [AoContract((int)IPCOpcode.Dismount)]
+        private class DismountMessage : IPCMessage
+        {
+            public override short Opcode => (short)IPCOpcode.Mount;
+        }
+    }
+}
--- Bots/Utilities/Mounter/Main.cs
+++ Bots/Utilities/Mounter/Main.cs
@@ -1,44 +1,51 @@
 ﻿using AOSharp.Common.GameData;
-using AOSharp.Common.GameData.UI;
 using AOSharp.Core;
+using AOSharp.Core.IPC;
 using AOSharp.Core.UI;
-using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.IO;
 
 namespace Mounter
 {
-    public class Main : AOPluginEntry
+    public partial class Main : AOPluginEntry
     {
+        public static IPCChannel IPCChannel;
 
         public override void Run()
         {
+            IPCChannel = new IPCChannel(Convert.ToByte(0));
+
+            try
+            {
+                IPCChannel.RegisterCallback((int)IPCOpcode.Mount, OnMountMessage);
+                IPCChannel.RegisterCallback((int)IPCOpcode.Dismount, OnDismountMessage);
+            }
+            catch (Exception e)
+            {
+                Chat.WriteLine(e);
+            }
 
             Chat.RegisterCommand("mount", (string command, string[] param, ChatWindow chatWindow) =>
             {
                 try
                 {
                     // Check if mounted.
-                    bool notMounted = true;
                     foreach (Buff buff in DynelManager.LocalPlayer.Buffs)
                     {
                         if (buff.Nanoline == NanoLine.Vehicles || buff.Name.Contains("Phasefront"))
                         {
                             buff.Remove();
-                            notMounted = false;
+                            IPCChannel.Broadcast(new DismountMessage());
+                            return;
                         }
                     }
                     // Mount if not mounted.
-                    if (notMounted)
+                    foreach (var spell in Spell.List.OrderByStackingOrder())
                     {
-                        foreach (var spell in Spell.List.OrderByStackingOrder())
+                        if (spell.Nanoline == NanoLine.Vehicles || spell.Name.Contains("Phasefront"))
                         {
-                            if (spell.Nanoline == NanoLine.Vehicles || spell.Name.Contains("Phasefront"))
-                            {
-                                spell.Cast();
-                                return;
-                            }
+                            spell.Cast();
+                            IPCChannel.Broadcast(new MountMessage());
+                            return;
                         }
                     }
                 }
@@ -50,6 +57,31 @@ namespace Mounter
 
             Chat.WriteLine("Mounter Loaded!");
             Chat.WriteLine("/mount to enter or exit your vehicle.");
+        }
+
+        public void OnMountMessage(int sender, IPCMessage msg)
+        {
+            if (!(msg is MountMessage)) return;
+            foreach (var spell in Spell.List.OrderByStackingOrder())
+            {
+                if (spell.Nanoline == NanoLine.Vehicles || spell.Name.Contains("Phasefront"))
+                {
+                    spell.Cast();
+                    return;
+                }
+            }
+        }
+
+        public void OnDismountMessage(int sender, IPCMessage msg)
+        {
+            if (!(msg is DismountMessage)) return;
+            foreach (Buff buff in DynelManager.LocalPlayer.Buffs)
+            {
+                if (buff.Nanoline == NanoLine.Vehicles || buff.Name.Contains("Phasefront"))
+                {
+                    buff.Remove();
+                }
+            }
         }
     }
 }
